0b3401186392a27e8e0802c016561255a0c48307
bhanuteja110 <163739352+Bhanutejanallamothu@users.noreply.github.com>
2026-03-11 12:28:07 +0530

    Create SeoSchema.tsx for SEO structured data
    Add structured data for SEO using JSON-LD format.

---
+++ src/components/SeoSchema.tsx
@@ -0,0 +1,21 @@
+<script type="application/ld+json">
+{
+ "@context": "https://schema.org",
+ "@type": "Person",
+ "name": "Bhanuteja",
+ "url": "https://nallamothu-bhanuteja.vercel.app/",
+ "jobTitle": "Freelance Full Stack Developer",
+ "sameAs": [
+   "https://www.linkedin.com/in/bhanuteja-nallamothu-4b8677315/"
+ ],
+ "knowsAbout": [
+   "UI Development",
+   "Full Stack Development",
+   "Cloud Architecture",
+   "React",
+   "Next.js",
+   "Node.js",
+   "AWS"
+ ]
+}
+</script>
